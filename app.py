54b068225207e91f151445bd98ad3699cec2cd26
Mike_Kalange <mikekalange@gmail.com>
2025-09-05 06:51:47 +0200

    Fix app entry point to use environment PORT variable for deployment

--- app.py
+++ app.py
@@ -122,4 +122,6 @@ def view_bill(bill_id):
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
